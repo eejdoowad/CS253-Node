76275ba43b47afbdd43b60119f990a5ff99f9b7e
eejdoowad <eejdoowad@gmail.com>
2016-02-27 18:26:09 -0500

    some stuff

--- test/hello.js
+++ test/hello.js
@@ -1,11 +1,35 @@
-/*jslint node:true */
-"use strict";
+var home_content = "And so the tale begins...";
+
+var content =
+    `<!doctype html>
+    <html>
+        <head
+            <title>lesson 1</title>
+        </head>
+        <body>
+            here we are and here we aren't
+            <form action="http://www.google.com/search">
+                <input name="q">
+                <input type="submit">
+            </form>
+        </body>
+    </html>`;
 
 var http = require("http");
 
 var server = http.createServer(function (request, response) {
-    response.writeHead(200, {"Content-Type" : "text/plain"});
-    response.end("Hello World 2");
+    if (request.method === "GET") {
+        if (request.url === "/") {
+            response.writeHead(200, {"Content-Type": "text/plain"});
+            response.end(home_content);
+        } else if (request.url === "/test") {
+            response.writeHead(200, {"Content-Type": "text/html"});
+            response.end(content);
+        } else {
+            response.writeHead(404, {"Content-Type": "text/html"});
+            response.end();
+        }
+    }
 });
 
 server.listen(8000);
