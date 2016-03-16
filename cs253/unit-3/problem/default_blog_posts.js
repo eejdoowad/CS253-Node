613d08eebdffeffb3e3410389a47d136dadbd875
eejdoowad <eejdoowad@gmail.com>
2016-03-15 21:50:35 -0400

    blog

---
+++ cs253/unit-3/problem/default_blog_posts.js
@@ -0,0 +1,88 @@
+'use strict';
+const post1 =
+`R. A. Villanueva
+
+At the columbarium dug
+by hand, a man points to where rock
+doves would be brought to nest, their eggs
+
+tended by priests, and the cave locked
+at sundown, guarded by hired
+knives. The flock meant meat for the dry
+
+times; saltpeter; yolks needed to bind
+portraits to walls, to raise a sky
+gilded with violets and myrrh.
+
+Tonight, my mother paints her nails
+black—a shade she names, “Dark Matter.”
+She numbers what’s left of her cells,
+
+tells us of this burning inside
+her knees, laughs a promise to fight.`;
+
+const post2 =
+`Amy Lowell, 1874 - 1925
+
+Blue through the window burns the twilight;
+  Heavy, through trees, blows the warm south wind.
+Glistening, against the chill, gray sky light,
+  Wet, black branches are barred and entwined.
+
+Sodden and spongy, the scarce-green grass plot
+  Dents into pools where a foot has been.
+Puddles lie spilt in the road a mass, not
+  Of water, but steel, with its cold, hard sheen.
+
+Faint fades the fire on the hearth, its embers
+  Scattering wide at a stronger gust.
+Above, the old weathercock groans, but remembers
+  Creaking, to turn, in its centuried rust.
+
+Dying, forlorn, in dreary sorrow,
+  Wrapping the mists round her withering form,
+Day sinks down; and in darkness to-morrow
+  Travails to birth in the womb of the storm.`;
+
+
+const post3 =
+`Letitia Elizabeth Landon
+
+. . . It is the last survivor of a race
+Strong in their forest-pride when I was young.
+I can remember when, for miles around,
+In place of those smooth meadows and corn-fields,
+There stood ten thousand tall and stately trees,
+Such as had braved the winds of March, the bolt
+Sent by the summer lightning, and the snow
+Heaping for weeks their boughs. Even in the depth
+Of hot July the glades were cool; the grass,
+Yellow and parched elsewhere, grew long and fresh,
+Shading wild strawberries and violets,
+Or the lark’s nest; and overhead the dove
+Had her lone dwelling, paying for her home
+With melancholy songs; and scarce a beech
+Was there without a honeysuckle linked
+Around, with its red tendrils and pink flowers;
+Or girdled by a brier-rose, whose buds
+Yield fragrant harvest for the honey-bee
+There dwelt the last red deer, those antler’d kings . . .
+But this is as dream,—the plough has pass’d
+Where the stag bounded, and the day has looked
+On the green twilight of the forest-trees.
+This oak has no companion! . . . . `;
+
+module.exports = {
+  post1: {
+    title: 'When Doves',
+    content: post1
+  },
+  post2: {
+    title: 'March Evening',
+    content: post2
+  },
+  post3: {
+    title: 'The Oak',
+    content: post3
+  }
+};
---
+++ cs253/unit-3/problem/setup_blog_db.js
@@ -0,0 +1,50 @@
+'use strict';
+let sqlite3 = require('sqlite3').verbose();
+let path = require('path');
+let db = new sqlite3.Database(path.join(__dirname, 'blog.db'));
+let default_blog_posts = require('./default_blog_posts');
+
+// Check if links table exists.
+// If it doesn't, create links table and add data
+
+db.serialize(() => {
+  db.get("SELECT name FROM sqlite_master WHERE type='table' AND name='blog'", (err, row) => {
+    if (err) {
+      console.log(err);
+    } else {
+      setupDB(row);
+    }
+    // db.close();
+  });
+});
+
+function setupDB (row) {
+  db.serialize(() => {
+    // check if database exists
+    if (row === undefined) {
+      // Create database
+      db.run(
+        'CREATE TABLE IF NOT EXISTS blog' +
+        '(id INTEGER PRIMARY KEY, title TEXT, time TEXT, content TEXT)',
+        (err) => {
+          if (err) console.log(err);
+        });
+      // insert default entries
+      addEntry(default_blog_posts.post1);
+      addEntry(default_blog_posts.post2);
+      addEntry(default_blog_posts.post3);
+    } else {
+      console.log('blog.sql already exists');
+    }
+  });
+}
+
+function addEntry (entry) {
+  db.run(
+    'INSERT INTO blog (title, content, time) VALUES (?, ?, CURRENT_TIMESTAMP)',
+    entry.title,
+    entry.content,
+    (err) => {
+      if (err) console.log(err);
+    });
+}
